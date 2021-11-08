2d09ab114c16af10022c5d3f334605fd697c4ae6
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-11-08 15:54:00 +0100

    Add slash to route

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello_flask():
     return "Hello Flask!"
 
 
-@app.route("/custom-greeting")
+@app.route("/custom-greeting/")
 def custom_greeting():
     greeting_value = request.args.get("greeting")
     return f"<h1>The greeting is: {greeting_value} </h1>"
