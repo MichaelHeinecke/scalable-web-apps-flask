6c3f573bed6ff81ea05b54182fb741eea6b3824d
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-11-08 15:52:04 +0100

    Add route for custom greeting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_flask():
     return "Hello Flask!"
+
+
+@app.route("/custom-greeting")
+def custom_greeting():
+    greeting_value = request.args.get("greeting")
+    return f"<h1>The greeting is: {greeting_value} </h1>"
 
 
 if __name__ == '__main__':
