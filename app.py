0f204216352dc62fab37a67aa683c5e3aaf90b6c
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-11-09 15:39:22 +0100

    Add macros.html, using_macro.html and route /macro

--- app.py
+++ app.py
@@ -38,14 +38,12 @@ def using_template():
 
 @app.route('/watch/<string:name>')
 def top_movies(name: str):
-    movie_list = [
-        'autopsy of jane doe',
-        'neon demon',
-        'ghost in a shell',
-        'kong: skull island',
-        'john wick 2',
-        'spiderman - homecoming'
-    ]
+    movie_list = ['autopsy of jane doe',
+                  'neon demon',
+                  'ghost in a shell',
+                  'kong: skull island',
+                  'john wick 2',
+                  'spiderman - homecoming']
 
     return render_template('movies.html',
                            movies=movie_list,
@@ -79,6 +77,18 @@ def filter_data():
                            movies=movies_dict,
                            name=None,
                            film='a christmas carol')
+
+
+@app.route('/macro')
+def jinja_macros():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('using_macro.html', movies=movies_dict)
 
 
 if __name__ == '__main__':
