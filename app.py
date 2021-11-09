5214a26b23dc1a27262b4c673b41ab7839fafc5c
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-11-09 14:30:17 +0100

    Add routes with float and int parameters

--- app.py
+++ app.py
@@ -21,5 +21,15 @@ def custom_greeting_without_query_string(name="No name"):
     return f"<h1>Hi {name}!</h1>"
 
 
+@app.route("/display-float/<float:num>")
+def display_float(num: float):
+    return f"<h1>The float passed is {str(num)}</h1>"
+
+
+@app.route("/add/<int:num1>/<int:num2>")
+def add_integers(num1: int, num2: int):
+    return f"<h1>{num1} + {num2} = {num1 + num2}</h1>"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
