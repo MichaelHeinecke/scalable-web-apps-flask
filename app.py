4a4a21d4d792202c085987aed31d9e9e68481537
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-11-09 15:03:46 +0100

    Add table.html and route /table

--- app.py
+++ app.py
@@ -52,5 +52,19 @@ def top_movies(name: str):
                            name=name)
 
 
+@app.route("/table/<string:name>")
+def movies_plus(name: str):
+    movies_dict = {"autopsy of jane doe": 02.14,
+                   "neon demon": 3.20,
+                   "ghost in a shell": 1.50,
+                   "kong: skull island": 3.50,
+                   "john wick 2": 02.52,
+                   "spiderman - homecoming": 1.48}
+
+    return render_template("table.html",
+                           movies=movies_dict,
+                           name=name)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
